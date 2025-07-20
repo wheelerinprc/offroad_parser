Update dataset.py: Fix a fatal bug - image and label do not match. Update eval.py: add dice loss calculation to monitor performance of model in training process Update train.py: add dice loss code as training loss but do not use because of unstable performance Update process and parse_json.py: now model parameter is read from a json in working directory

eval.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
+from diceloss import SoftDiceLoss
+soft_dice_loss = SoftDiceLoss(21, backprop=False)
 
 def eval_model(model, logger, dataloader, device, test_step):
     total_test_loss = 0
+    total_dice_loss = 0
     test_count = 0
     criterion = nn.CrossEntropyLoss()
     model.eval()
@@ -12,9 +15,15 @@ def eval_model(model, logger, dataloader, device, test_step):
             image = image.to(device)
             label = label.to(device)
             output = model(image)
+
+            #calculate mIoU
+            dice_loss = soft_dice_loss(output, label)
+            total_dice_loss += dice_loss
+
             label = label.squeeze(1).long()
             loss = criterion(output, label)
             total_test_loss += loss.item()
             test_count += 1
-        print("Loss on train data: {}".format(total_test_loss / test_count))
+        print(f"Loss on train data - CrossEntropy Loss: {total_test_loss / test_count}, Dice Loss: {total_dice_loss / test_count}")
         logger.add_scalar('Test loss', total_test_loss / test_count, test_step)
+        logger.add_scalar('Test mIoU', total_dice_loss / test_count, test_step)

process.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 import unet
 from torch.utils.tensorboard import SummaryWriter
 import train
-from torchvision import transforms
+from utils.dataset_check import check_dataset
 
 if __name__=='__main__':
     train_images = "./Rellis_3D_pylon_camera_node_480/train"
@@ -22,11 +22,15 @@ if __name__=='__main__':
         exit(-1)
     working_dir = sys.argv[1]
     print("Working directory: ", working_dir)
+    if not os.path.exists(working_dir):
+        print("Error - Working directory does not exist")
+        exit(-1)
     json_parse = JsonParser(working_dir)
-
 
     print("Dataset reading ...")
     train_data = dataset.MyDataset(train_images, train_labels)
+    # check_dataset(train_data, working_dir)
+
     train_loader = DataLoader(train_data, batch_size=json_parse.train_batch, shuffle=True)
     val_data = dataset.MyDataset(val_images, val_labels)
     val_loader = DataLoader(val_data, batch_size=1, shuffle=True)
@@ -46,6 +50,6 @@ if __name__=='__main__':
                       out_channel=json_parse.class_num)
     model.to(device)
 
-    train.train_model(model, logger, train_loader, val_loader, device, working_dir, json_parse.model_name)
+    train.train_model(model, logger, train_loader, val_loader, device, working_dir, json_parse, num_epochs=100)
     logger.close()
 

train.py
@@ -1,14 +1,31 @@
 import os.path
 import torch
 import torch.nn as nn
+from mmdet.models import cross_entropy
+
+from diceloss import SoftDiceLoss
 from eval import  eval_model
 
-scaler = torch.cuda.amp.GradScaler()
-def train_model(model, logger, train_dataloader, eval_dataloader, device, working_dir, model_name, num_epochs=50, lr=0.001):
-    criterion = nn.CrossEntropyLoss()
+# scaler = torch.cuda.amp.GradScaler(init_scale=2056)
+
+def train_model(model, logger, train_dataloader, eval_dataloader, device, working_dir, configuration, num_epochs=50, lr=0.0001):
+    loss_weight = torch.tensor(configuration.class_weight).to(device)
+    criterion = nn.CrossEntropyLoss(weight=loss_weight)
+    soft_dice_loss = SoftDiceLoss(configuration.class_num)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     total_train_step = 0
     total_test_step = 0
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer,
+        milestones=[num_epochs//2, num_epochs//1],  # 在epoch 30和80时调整
+        gamma=0.1
+    )
+
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.xavier_uniform_(m.weight)  # 使用Xavier初始化参数
+
     for epoch in range(num_epochs):
         model.train() #
         for images, labels in train_dataloader:
@@ -18,22 +35,38 @@ def train_model(model, logger, train_dataloader, eval_dataloader, device, workin
             optimizer.zero_grad()
             with torch.cuda.amp.autocast():
                 outputs= model(images)
+                # dice_loss = soft_dice_loss(outputs, labels) * 2
                 labels = labels.squeeze(1).long()
                 # print("output.shape:",outputs.shape)
                 loss = criterion(outputs, labels)
-
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
+                # loss = torch.add(dice_loss, cross_entropy_loss)
+            # scaler.scale(loss).backward()
+            # scaler.step(optimizer)
+            # scaler.update()
+            loss.backward()
+            optimizer.step()
             total_train_step += 1
+
             if (total_train_step + 1) % 10 == 0:
+                # print(f'Epoch [{epoch + 1}/{num_epochs}], train step: {total_train_step}, loss: {loss.item():.4f}, '
+                #       f'dice loss: {dice_loss.item():.4f}, cross entropy loss: {cross_entropy_loss.item():.4f}')
                 print(f'Epoch [{epoch + 1}/{num_epochs}], train step: {total_train_step}, loss: {loss.item():.4f}')
+                # logger.add_scalar('train cross loss', cross_entropy_loss.item(), total_train_step)
+                # logger.add_scalar('train dice loss', dice_loss.item(), total_train_step)
                 logger.add_scalar('train loss', loss.item(), total_train_step)
+                for name,param in model.named_parameters():
+                    if param.grad is not None:
+                        # print(f"{name} grad: {param.grad.norm().item()}")
+                        logger.add_scalar(f'{name} grad', param.grad.norm().item(), total_train_step)
+
+        scheduler.step()
         eval_model(model, logger, eval_dataloader, device, total_test_step)
-        model_path = os.path.join(working_dir, model_name)
-        if (total_test_step + 1) % 10 == 0:
-            torch.save(model.state_dict(), model_path)
         total_test_step += 1
+        if total_test_step % 1 == 0:
+            # model_name = configuration.model_name + str(total_test_step) + ".pth"
+            model_name = "model_" + str(total_test_step) + ".pth"
+            model_path = os.path.join(working_dir, model_name)
+            torch.save(model.state_dict(), model_path)
 
 
 